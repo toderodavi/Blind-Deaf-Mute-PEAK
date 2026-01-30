0ce54e3a3791ca20fc422982f76a41af0073f62e
toderodavi <estrela.alem@gmail.com>
2026-01-30 15:21:57 -0300

    docs: deleted some of the template comments

--- src/BlindDeafMutePEAK/Plugin.cs
+++ src/BlindDeafMutePEAK/Plugin.cs
@@ -5,16 +5,6 @@ using HarmonyLib;
 
 namespace BlindDeafMutePEAK;
 
-// Here are some basic resources on code style and naming conventions to help
-// you in your first CSharp plugin!
-// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
-// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
-// https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-namespaces
-
-// This BepInAutoPlugin attribute comes from the Hamunii.BepInEx.AutoPlugin
-// NuGet package, and it will generate the BepInPlugin attribute for you!
-// For more info, see https://github.com/Hamunii/BepInEx.AutoPlugin
-
 // That is an ATTRIBUTE.
 [BepInAutoPlugin]
 public partial class Plugin : BaseUnityPlugin
